ae34bca6d97a67f52791d679e4067192231c7a65
jam <279956327@qq.com>
2017-08-05 18:57:58 +0800

    update database support

--- TC58/pipelines.py
+++ TC58/pipelines.py
@@ -8,14 +8,21 @@ import pymysql
 
 class Tc58Pipeline(object):
     def __init__(self):
-        self.conn = pymysql.connect(host = "127.0.0.1", user = "root", passwd = "0", db = "ScrapyTongcheng")
+        self.conn = pymysql.connect(host = "127.0.0.1", user = "root", passwd = "0", db = "tongcheng")
 
     def process_item(self, item, spider):
-        title = item["title"][0]
-        link = item["link"][0]
-        sql = "INSERT INTO result(title, link) VALUES('dafdadf', '"+link+"')"
-        # sql = "INSERT INTO result(link) VALUES('"+link+"')"
+        title = ''.join(item["title"])
+        price = ''.join(item["price"])
+        houseType = ''.join(item["houseType"])
+        link = ''.join(item["link"])
+        detail = ''.join(item["detail"])
+        method = ''.join(item["method"])
+        commity = ''.join(item["commity"])
+
+
+        sql = "INSERT INTO result(title, price, houseType, link, detail, method, commity) VALUES('"+title+"', '"+price+"', '"+houseType+"', '"+link+"', '"+detail+"', '"+method+"', '"+commity+"')"
         self.conn.query(sql)
+        self.conn.commit()
         return item
 
     def close_spider(self, spider):
--- TC58/spiders/tc58.py
+++ TC58/spiders/tc58.py
@@ -6,38 +6,41 @@ class Tc58Spider(scrapy.Spider):
     name = 'tc58'
     allowed_domains = ['nj.58.com']
     start_urls = ['http://nj.58.com/jiangning/hezu/0/?PGTID=0d30000a-00b9-517a-920c-54a3803d2cf3&ClickID=4']
+    # start_urls = ['http://nj.58.com/jianye/hezu/0/?PGTID=0d30000a-00b9-58ca-e32f-eab1476fa03a&ClickID=3']
 
     detailLink = 'www.nj.58.com'
     pageNum = 1
 
     def parse(self, response):
-    	lis = response.xpath('/html/body/div[3]/div[1]/div[5]/div[2]/ul/li')
-    	for li in lis:
-    		if len(li.xpath('div[2]/h2/a/@href').extract()) != 0:
-	        	link = li.xpath('div[2]/h2/a/@href').extract()[0]
-	        	self.detailLink = link
-	        	yield scrapy.Request(url = link, callback = self.parseDetail)
-#    	print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+        lis = response.xpath('/html/body/div[3]/div[1]/div[5]/div[2]/ul/li')
+        count = 0
+        for li in lis:
+            if len(li.xpath('div[2]/h2/a/@href').extract()) != 0 and count < 100:
+                link = li.xpath('div[2]/h2/a/@href').extract()[0]
+                count = count + 1
+                self.detailLink = link
+                yield scrapy.Request(url = link, callback = self.parseDetail)
+#       print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
  
  # set the pages to crawl
-    	if self.pageNum < 2:
-    		self.pageNum += 1
-#    		print('###############################')
-    		if len(response.xpath('//*[@id="bottom_ad_li"]/div[2]/a[12]/@href').extract()) != 0:
-	    		nextPageLink = (response.xpath('//*[@id="bottom_ad_li"]/div[2]/a[12]/@href').extract()[0])
-	    		yield scrapy.Request(url = nextPageLink, callback = self.parse)
+        if self.pageNum < 10:
+            self.pageNum += 1
+#           print('###############################')
+            if len(response.xpath('//*[@id="bottom_ad_li"]/div[2]/a[12]/@href').extract()) != 0:
+                nextPageLink = (response.xpath('//*[@id="bottom_ad_li"]/div[2]/a[12]/@href').extract()[0])
+                yield scrapy.Request(url = nextPageLink, callback = self.parse)
 
     def parseDetail(self, response):
 
-    	lis = response.xpath('/html/body/div[4]')
-    	for li in lis:
-    		item = Tc58Item()
-    		item['title'] = li.xpath('div[1]/h1/text()').extract()
-    		item['price'] = li.xpath('div[2]/div[2]/div[1]/div[1]/div/span[1]/b/text()').extract()
-    		item['houseType'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[2]/span[2]/text()').extract()
-    		item['detail'] = li.xpath('div[3]/div[1]/div[1]/p/text()').extract()
-    		item['method'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[1]/span[2]/text()').extract()
-    		item['link'] = self.detailLink
-    		item['commity'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[4]/span[2]/a/text()').extract()
+        lis = response.xpath('/html/body/div[4]')
+        for li in lis:
+            item = Tc58Item()
+            item['title'] = li.xpath('div[1]/h1/text()').extract()
+            item['price'] = li.xpath('div[2]/div[2]/div[1]/div[1]/div/span[1]/b/text()').extract()
+            item['houseType'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[2]/span[2]/text()').extract()
+            item['detail'] = li.xpath('div[3]/div[1]/div[1]/p/text()').extract()
+            item['method'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[1]/span[2]/text()').extract()
+            item['link'] = self.detailLink
+            item['commity'] = li.xpath('div[2]/div[2]/div[1]/div[1]/ul/li[4]/span[2]/a/text()').extract()
 
-    		yield item
+            yield item
